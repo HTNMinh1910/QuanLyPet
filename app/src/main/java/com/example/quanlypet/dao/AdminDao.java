0439e0a99a5957e3ea6dc1d9b338800fa27efb0b
HaMinhcomet <100187061+HTNMinh1910@users.noreply.github.com>
2022-11-08 12:59:02 +0700

    commit2

--- app/src/main/java/com/example/quanlypet/dao/AdminDao.java
+++ app/src/main/java/com/example/quanlypet/dao/AdminDao.java
@@ -1,11 +1,12 @@
 package com.example.quanlypet.dao;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
 import java.util.List;
-
+@Dao
 public interface AdminDao {
     @Insert
     void insert(Object object);
