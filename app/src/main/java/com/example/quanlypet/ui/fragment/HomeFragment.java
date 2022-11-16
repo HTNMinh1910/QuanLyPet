71c2a35998dbe98d231c7c89d8ea82081bc491d7
tungndph27567 <116142197+tungndph27567@users.noreply.github.com>
2022-11-16 22:47:52 +0700

    tungndph27567 commit giao diện booking

--- app/src/main/java/com/example/quanlypet/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/quanlypet/ui/fragment/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.quanlypet.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.quanlypet.R;
+import com.example.quanlypet.ui.activity.AddBooking;
 
 
 /**
@@ -18,6 +21,12 @@ import com.example.quanlypet.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    private LinearLayout linerBooking;
+    private LinearLayout linerAmbulance;
+    private LinearLayout linerMess;
+
+
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -44,6 +53,16 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        linerBooking = (LinearLayout) view.findViewById(R.id.liner_booking);
+        linerAmbulance = (LinearLayout) view.findViewById(R.id.liner_ambulance);
+        linerMess = (LinearLayout) view.findViewById(R.id.liner_mess);
+        linerBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddBooking.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
