2c16ef29d323d4c30432223ff12878f67d0f2eb9
HaMinhcomet <100187061+HTNMinh1910@users.noreply.github.com>
2022-11-16 15:53:05 +0700

    commit fragment account

--- app/src/main/java/com/example/quanlypet/ui/fragment/UsersFragment.java
+++ app/src/main/java/com/example/quanlypet/ui/fragment/UsersFragment.java
@@ -30,7 +30,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-public class UsersFragment extends Fragment implements UsersAdapter.Callback {
+public class UsersFragment extends Fragment {
     private UsersAdapter usersAdapter;
     private ArrayList<UsersObj> list = new ArrayList<>();
 
@@ -46,7 +46,6 @@ public class UsersFragment extends Fragment implements UsersAdapter.Callback {
     private RadioButton rdoFemaleEdit;
     private TextView tvEditUsers;
     private TextView tvCancelEditUsers;
-
 
 
     public UsersFragment() {
@@ -72,88 +71,6 @@ public class UsersFragment extends Fragment implements UsersAdapter.Callback {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        rcvListUsers = (RecyclerView) view.findViewById(R.id.rcv_listUsers);
-        bbtnAddUsers = (FloatingActionButton) view.findViewById(R.id.bbtn_addUsers);
-        usersAdapter = new UsersAdapter(getActivity(),this);
-        LoadData();
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
-        rcvListUsers.setLayoutManager(linearLayoutManager);
-        rcvListUsers.setAdapter(usersAdapter);
-
-        bbtnAddUsers.setOnClickListener(v->{
-            startActivity(new Intent(getContext(), SignupUsersActivity.class));
-        });
-    }
-    public void LoadData(){
-        list = (ArrayList<UsersObj>) UsersDB.getInstance(getActivity()).Dao().getAllData();
-        usersAdapter.setData(list);
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        LoadData();
     }
 
-    @Override
-    public void editUsers(UsersObj usersObj) {
-        final Dialog dialog = new Dialog(getActivity());
-        dialog.setCancelable(false);
-        dialog.setContentView(R.layout.dialog_editusers);
-        Window window = dialog.getWindow();
-        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        if (dialog != null && dialog.getWindow() != null){
-            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        }
-
-        edImportnameUsers = (EditText) dialog.findViewById(R.id.ed_importnameUsers);
-        edFullnameUsers = (EditText) dialog.findViewById(R.id.ed_fullnameUsers);
-        edEmailUsers = (EditText) dialog.findViewById(R.id.ed_emailUsers);
-        edPhoneUsers = (EditText) dialog.findViewById(R.id.ed_phoneUsers);
-        rdoMaleEdit = (RadioButton) dialog.findViewById(R.id.rdo_maleEdit);
-        rdoFemaleEdit = (RadioButton) dialog.findViewById(R.id.rdo_femaleEdit);
-        edStatusUsers = (EditText) dialog.findViewById(R.id.ed_statusUsers);
-        tvEditUsers = (TextView) dialog.findViewById(R.id.tv_editUsers);
-        tvCancelEditUsers = (TextView) dialog.findViewById(R.id.tv_cancelEditUsers);
-
-        edImportnameUsers.setText(usersObj.getImport_name());
-        edFullnameUsers.setText(usersObj.getFull_name());
-        edEmailUsers.setText(usersObj.getEmail());
-        edPhoneUsers.setText(usersObj.getPhone());
-        if (usersObj.getGender() == 0){
-            rdoMaleEdit.setChecked(true);
-        } else if(usersObj.getGender() == 1){
-            rdoFemaleEdit.setChecked(true);
-        }
-
-        tvEditUsers.setOnClickListener(v->{
-            String importName = edImportnameUsers.getText().toString().trim();
-            String fullName = edFullnameUsers.getText().toString().trim();
-            String email = edEmailUsers.getText().toString().trim();
-            String phone = edPhoneUsers.getText().toString().trim();
-            int gender = 0;
-            if (rdoMaleEdit.isChecked() == true){
-                gender = 0;
-            } else if (rdoFemaleEdit.isChecked() == true){
-                gender = 1;
-            }
-            int status = Integer.parseInt(edStatusUsers.getText().toString().trim());
-            if (importName .isEmpty() || fullName.isEmpty() || email.isEmpty() || phone.isEmpty() || edStatusUsers.getText().toString().trim().isEmpty()){
-                Toast.makeText(getActivity(), "Không được để trống!", Toast.LENGTH_SHORT).show();
-            } else {
-                usersObj.setImport_name(importName);
-                usersObj.setFull_name(fullName);
-                usersObj.setEmail(email);
-                usersObj.setPhone(phone);
-                usersObj.setGender(gender);
-                UsersDB.getInstance(getActivity()).Dao().edit(usersObj);
-                Toast.makeText(getActivity(), "Sửa thành công!", Toast.LENGTH_SHORT).show();
-                LoadData();
-                dialog.cancel();
-            }
-        });
-        tvCancelEditUsers.setOnClickListener(v1->{
-            dialog.cancel();
-        });
-        dialog.show();
-    }
 }
