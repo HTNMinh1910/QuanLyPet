3356cf51fa5284394760c88a18c48d4d12bf9961
HaMinhcomet <100187061+HTNMinh1910@users.noreply.github.com>
2022-11-24 19:05:31 +0700

    commit information users

--- app/src/main/java/com/example/quanlypet/ui/activity/UsersInforActivity.java
+++ app/src/main/java/com/example/quanlypet/ui/activity/UsersInforActivity.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.quanlypet.R;
+import com.example.quanlypet.database.AdminDB;
 import com.example.quanlypet.database.UsersDB;
+import com.example.quanlypet.model.AdminObj;
 import com.example.quanlypet.model.UsersObj;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -30,12 +32,6 @@ public class UsersInforActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_information);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("Users_info",MODE_PRIVATE);
-        String username = sharedPreferences.getString("Username","");
-
-
-        UsersObj usersObj = UsersDB.getInstance(this).Dao().getIdUsers(username);
-
         idTollBar = findViewById(R.id.id_tollBar);
         edUsername = findViewById(R.id.ed_username);
         edFullnameUser = findViewById(R.id.ed_fullnameUser);
@@ -44,43 +40,69 @@ public class UsersInforActivity extends AppCompatActivity {
         edGenderUsers = findViewById(R.id.ed_genderUsers);
         btnUpdateUser = findViewById(R.id.btn_updateUser);
         tvThongbao = findViewById(R.id.tv_thongbao);
-
         tvThongbao.setVisibility(View.INVISIBLE);
 
         setSupportActionBar(idTollBar);
         getSupportActionBar().setTitle("Personal Information");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        edUsername.setText(username);
-        edFullnameUser.setText(usersObj.getFull_name());
-        edPhoneUsers.setText(usersObj.getPhone());
-        edEmailUsers.setText(usersObj.getEmail());
-        if (usersObj.getGender() == 0){
-            edGenderUsers.setText("Male");
-        } else {
-            edGenderUsers.setText("Female");
-        }
+        SharedPreferences sharedPreferences = getSharedPreferences("Users_info", MODE_PRIVATE);
+        String username = sharedPreferences.getString("Username", "");
+        if (username.equals("Admin")) {
+            AdminObj adminObj = AdminDB.getInstance(this).Dao().getIdAdmin(username);
+            edUsername.setText(username);
+            edFullnameUser.setText(adminObj.getFull_name());
+            edPhoneUsers.setText("Null");
+            edEmailUsers.setText(adminObj.getEmail());
+            edGenderUsers.setText("Null");
+            btnUpdateUser.setOnClickListener(view -> {
+                String usernamenew = edUsername.getText().toString();
+                String fullname = edFullnameUser.getText().toString().trim();
+                String email = edEmailUsers.getText().toString().trim();
 
-        btnUpdateUser.setOnClickListener(v->{
-            String usernamenew = edUsername.getText().toString();
-            String fullname = edFullnameUser.getText().toString().trim();
-            String email = edEmailUsers.getText().toString().trim();
-            String phone = edPhoneUsers.getText().toString().trim();
-            int gender = edGenderUsers.getText().toString().trim().equals("Male")?0:1;
-            if (usernamenew.isEmpty()||fullname.isEmpty()||email.isEmpty()||phone.isEmpty()){
-                tvThongbao.setVisibility(View.VISIBLE);
+                if (usernamenew.isEmpty() || fullname.isEmpty() || email.isEmpty()) {
+                    tvThongbao.setVisibility(View.VISIBLE);
+                } else {
+                    adminObj.setImport_name(usernamenew);
+                    adminObj.setFull_name(fullname);
+                    adminObj.setEmail(email);
+                    AdminDB.getInstance(this).Dao().edit(adminObj);
+                    tvThongbao.setText("Update successfully");
+                    Toast.makeText(this, "Update successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+        }else {
+            UsersObj usersObj = UsersDB.getInstance(this).Dao().getIdUsers(username);
+            edUsername.setText(username);
+            edFullnameUser.setText(usersObj.getFull_name());
+            edPhoneUsers.setText(usersObj.getPhone());
+            edEmailUsers.setText(usersObj.getEmail());
+            if (usersObj.getGender() == 0) {
+                edGenderUsers.setText("Male");
             } else {
-                usersObj.setImport_name(usernamenew);
-                usersObj.setFull_name(fullname);
-                usersObj.setEmail(email);
-                usersObj.setPhone(phone);
-                usersObj.setGender(gender);
-                UsersDB.getInstance(this).Dao().edit(usersObj);
-                tvThongbao.setText("Update successfully");
-                Toast.makeText(this, "Update successfully", Toast.LENGTH_SHORT).show();
-                finish();
+                edGenderUsers.setText("Female");
             }
-        });
-
+            btnUpdateUser.setOnClickListener(v -> {
+                String usernamenew = edUsername.getText().toString();
+                String fullname = edFullnameUser.getText().toString().trim();
+                String email = edEmailUsers.getText().toString().trim();
+                String phone = edPhoneUsers.getText().toString().trim();
+                int gender = edGenderUsers.getText().toString().trim().equals("Male") ? 0 : 1;
+                if (usernamenew.isEmpty() || fullname.isEmpty() || email.isEmpty() || phone.isEmpty()) {
+                    tvThongbao.setVisibility(View.VISIBLE);
+                } else {
+                    usersObj.setImport_name(usernamenew);
+                    usersObj.setFull_name(fullname);
+                    usersObj.setEmail(email);
+                    usersObj.setPhone(phone);
+                    usersObj.setGender(gender);
+                    UsersDB.getInstance(this).Dao().edit(usersObj);
+                    tvThongbao.setText("Update successfully");
+                    Toast.makeText(this, "Update successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+        }
     }
 }
